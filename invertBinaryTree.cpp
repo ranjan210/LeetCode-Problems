bf8b7966e9e86bfaece6640712ba2f87fbd6cdf7
Ranjan <mynamerkt3@gmail.com>
2023-06-09 20:18:49 +0530

    Invert Binary Tree

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,11 @@
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return root;
+        }
+        TreeNode* temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};
